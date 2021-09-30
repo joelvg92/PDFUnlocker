400d3986157d8a6fafbbb96507f523d15b57fdef
Joel Varghese <solidrock92@gmail.com>
2021-09-29 23:06:33 -0400

    Added pdfs path

--- src/main/java/com/pdf/pdfassembler/utils/FIleUtils.java
+++ src/main/java/com/pdf/pdfassembler/utils/FIleUtils.java
@@ -19,5 +19,19 @@ public class FIleUtils {
             System.err.println(x);
         }
     }
+    public InputStream getFileFromResourceAsStream(String fileName) {
+
+        // The class loader that loaded the class
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(fileName);
+
+        // the stream holding the file content
+        if (inputStream == null) {
+            throw new IllegalArgumentException("file not found! " + fileName);
+        } else {
+            return inputStream;
+        }
+
+    }
 
 }
--- src/main/java/com/pdf/pdfassembler/utils/UnlockPDF.java
+++ src/main/java/com/pdf/pdfassembler/utils/UnlockPDF.java
@@ -6,17 +6,26 @@ import com.itextpdf.text.pdf.PdfWriter;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class UnlockPDF {
     public InputStream unlock(String password, MultipartFile file) throws Exception {
         PdfReader.unethicalreading = true;
         PdfReader reader;
+        FIleUtils fIleUtils = new FIleUtils();
         if (password == null) {
             reader = new PdfReader(file.getBytes());
         } else {
              reader = new PdfReader(file.getBytes(), password.getBytes());
         }
-        String unlockedFile ="pdfs/"+file.getOriginalFilename();
+        Path source = Paths.get(this.getClass().getResource("/").getPath());
+        Path pdfFolder = Paths.get(source.toAbsolutePath() + "/pdfs/");
+        if(!Files.exists(pdfFolder)){
+            Files.createDirectories(pdfFolder);
+        }
+        String unlockedFile = pdfFolder.toString() + '/' + file.getOriginalFilename();
         PdfEncryptor.encrypt(reader, new FileOutputStream(unlockedFile), null,
                 null, PdfWriter.ALLOW_ASSEMBLY | PdfWriter.ALLOW_COPY
                         | PdfWriter.ALLOW_DEGRADED_PRINTING | PdfWriter.ALLOW_FILL_IN
@@ -24,6 +33,7 @@ public class UnlockPDF {
                         | PdfWriter.ALLOW_PRINTING | PdfWriter.ALLOW_SCREENREADERS, false);
         File pdfFile = new File(unlockedFile);
         InputStream inputStream = new BufferedInputStream(new FileInputStream(pdfFile));
+        //InputStream inputStream = fIleUtils.getFileFromResourceAsStream(unlockedFile);
         FIleUtils.deleteFIle(unlockedFile);
         return inputStream;
     }
